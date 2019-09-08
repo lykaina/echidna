70d6c6ee4ef17ea40e50c46ad1dd1d7474f17484
Sara Berman <mjbermanator@gmail.com>
2019-09-08 11:25:53 -0400

    Changed 'K' op to match 'G' op's current code. I plan on changing 'G' op to something else.

--- src/echidna.c
+++ src/echidna.c
@@ -162,7 +162,7 @@ int cmds(unsigned char a[INSIZE], unsigned int wcount){
         putchar(pval(a[1],a[2],a[3],a[4],a[5]));
         break;
     case 'K':
-        for(i=0;i<256;i++) mem[(hextoval(a[1])*16+hextoval(a[2]))*256+i]=0;
+        return 2;
         break;
     case 'L': // 1-bit bitwise
         mem[mval(a[1],a[2],a[3],a[4],a[5])]=cmd_l(pval(a[6],a[7],a[8],a[9],a[10]),pval(a[11],a[12],a[13],a[14],a[15]),pval(a[16],a[17],a[18],a[19],a[20]));
